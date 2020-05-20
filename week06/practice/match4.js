4297063654fd8f48a75a7fbd89613cd406eabaac
luohuidong <luohuidong01@126.com>
2020-05-21 01:46:38 +0800

    update

--- week06/practice/match4.js
+++ week06/practice/match4.js
@@ -1,4 +1,48 @@
-function match(string) {
-    let start = start; // 一开始会有一个 start 状态
+class Match {
+    match(string) {
+        let state = this.start;
+        for(let c of string) {
+            state = state(c);
+        }
+        return state === this.end;
+    }
+
+    start(c) {
+        if (c === "a") {
+            return this.foundA;
+        } else {
+            return start;
+        }
+    }
+
+    end(c) {
+        return end;
+    }
+
+    foundA(c) {
+        if (c === "b") {
+            return this.foundB;
+        } else {
+            return this.start;
+        }
+    }
+
+    foundB(c) {
+        if (c === "c") {
+            return this.foundC;
+        } else {
+            return this.start;
+        }
+    }
     
+    foundC(c) {
+        if (c === "d") {
+            return this.foundD;
+        } else {
+            return this.start;
+        }
+    }
 }
+
+const instance = new Match()
+console.log(instance.match('I abm groot'))
